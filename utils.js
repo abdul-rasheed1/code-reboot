6f0ff4d8446f00292cd02c92ee7f87d1bd5ca21a
Rash <rasheedjawula18@gmail.com>
2025-08-24 11:44:36 +0000

    cretated a function that wraps other functions with log messages

--- utils.js
+++ utils.js
@@ -11,3 +11,17 @@ export const mapTasks = (mapper) =>{
         return arr.map(mapper);
     }
 }
+
+export const compose = (f,g) =>{
+    return (x) => f(g(x));
+}
+
+export const withLogging = (fn,label) => {
+    return (...args) => {
+        console.log(`START ${label}`);
+        const res = fn(...args)
+        console.log(`END ${label}`);
+        return res;
+    }
+
+}
